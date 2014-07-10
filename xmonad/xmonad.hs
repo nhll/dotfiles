0e2b28bbc98c626cfbf428389baee822e8f72877
Nik Hille <nhille030@gmail.com>
2014-07-10 22:53:19 +0200

    Xmonad: implemented custom log hook and simple PP

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -7,23 +7,29 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 main = do
-    xmproc <- spawnPipe "xmobar"
+    xmobar <- spawnPipe "xmobar"
     xmonad $ defaultConfig
-        { manageHook = manageDocks <+> manageHook defaultConfig
-        , layoutHook = avoidStruts  $  layoutHook defaultConfig
-        , logHook = dynamicLogWithPP xmobarPP
-                        { ppOutput = hPutStrLn xmproc
-                        , ppTitle = xmobarColor "#90a959" "" . shorten 50
-                        }
-        , modMask = mod4Mask
-        , terminal = "urxvtc"
-        , borderWidth = 2
-        , normalBorderColor = "#202020"
-        , focusedBorderColor = "#90a959"
+        { manageHook         = manageDocks <+> manageHook defaultConfig
+        , layoutHook         = avoidStruts  $  layoutHook defaultConfig
+        , logHook            = myLogHook xmobar
+        , modMask            = mod4Mask
+        , terminal           = "urxvtc"
+        , borderWidth        = 2
+        , normalBorderColor  = "#202020"
+        , focusedBorderColor = "#6a9fb5"
         } `additionalKeys`
         [ ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -10")
-        , ((0, xF86XK_MonBrightnessUp), spawn "xbacklight +10")
-        , ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master 2%- unmute")
-        , ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master 2%+ unmute")
-        , ((0, xF86XK_AudioMute), spawn "amixer set Master toggle")
+        , ((0, xF86XK_MonBrightnessUp),   spawn "xbacklight +10")
+        , ((0, xF86XK_AudioLowerVolume),  spawn "amixer set Master 2%- unmute")
+        , ((0, xF86XK_AudioRaiseVolume),  spawn "amixer set Master 2%+ unmute")
+        , ((0, xF86XK_AudioMute),         spawn "amixer set Master toggle")
         ]
+
+myLogHook h = dynamicLogWithPP $ defaultPP 
+    { ppOutput = hPutStrLn h
+    , ppOrder  = \(ws:l:t:_) -> [ws]
+    , ppCurrent = xmobarColor "#6a9fb5" ""
+    , ppUrgent  = xmobarColor "#ac4142" ""
+    , ppHiddenNoWindows = xmobarColor "#282828" ""
+    , ppWsSep   = " "
+    }
