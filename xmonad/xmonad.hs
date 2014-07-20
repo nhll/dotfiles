e6373f8654673d06224f3cf48e45e50a2c85a07b
Nik Hille <nhille030@gmail.com>
2014-07-20 19:15:42 +0200

    Xmonad: Sane way for custom keybindings + dmenu layout

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,9 +1,11 @@
-import Graphics.X11.ExtraTypes.XF86
+import qualified Data.Map as M
+
 import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig
+import Graphics.X11.ExtraTypes.XF86
 import System.IO
 
 main = do
@@ -17,13 +19,8 @@ main = do
         , borderWidth        = 2
         , normalBorderColor  = "#202020"
         , focusedBorderColor = "#6a9fb5"
-        } `additionalKeys`
-        [ ((0, xF86XK_MonBrightnessDown), spawn "xbacklight -10")
-        , ((0, xF86XK_MonBrightnessUp),   spawn "xbacklight +10")
-        , ((0, xF86XK_AudioLowerVolume),  spawn "amixer set Master 2%- unmute")
-        , ((0, xF86XK_AudioRaiseVolume),  spawn "amixer set Master 2%+ unmute")
-        , ((0, xF86XK_AudioMute),         spawn "amixer set Master toggle")
-        ]
+        } 
+        `additionalKeysP` myKeys
 
 myLogHook h = dynamicLogWithPP $ defaultPP 
     { ppOutput = hPutStrLn h
@@ -33,3 +30,15 @@ myLogHook h = dynamicLogWithPP $ defaultPP
     , ppHiddenNoWindows = xmobarColor "#282828" ""
     , ppWsSep   = " "
     }
+
+myKeys =
+    [ ("M-p",                       spawn "dmenu_run -h 42 -fn Inconsolata-16")
+    -- Multimedia keys
+    , ("<XF86AudioLowerVolume>",    spawn "amixer set Master 2%- unmute")
+    , ("<XF86AudioRaiseVolume>",    spawn "amixer set Master 2%+ unmute")
+    , ("<XF86AudioMute>",           spawn "amixer set Master toggle")
+    , ("<XF86MonBrightnessDown>",   spawn "xbacklight -10")
+    , ("<XF86MonBrightnessUp>",     spawn "xbacklight +10")
+    , ("<XF86KbdBrightnessDown>",   spawn "sudo /home/nik/bin/keyboard-backlight down")
+    , ("<XF86KbdBrightnessUp>",     spawn "sudo /home/nik/bin/keyboard-backlight up")
+    ]
