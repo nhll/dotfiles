b0f75a2fca3a2a91a193c1847cca25b1fa33e56d
Nik Hille <nik.hille@macio.de>
2017-06-27 14:49:53 +0200

    i3: Add exit code to theme switcher

--- i3/set-theme.sh
+++ i3/set-theme.sh
@@ -9,6 +9,8 @@ if [ -f $THEME_FILE ]; then
   echo "Switching to $1 theme..."
   cat $THEME_FILE $CONFIG_DIR/*.config > $I3_DIR/config
   i3-msg restart &> /dev/null
+  exit 0
 else
   echo "File $THEME_FILE does not exist."
+  exit 1
 fi
