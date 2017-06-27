bf1e24ba9071933ef16a6a04dd7cc21c124244ab
Nik Hille <nik.hille@macio.de>
2017-06-27 14:47:24 +0200

    Add i3 config with theme switcher

---
+++ i3/set-theme.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+I3_DIR="/home/nik/.i3"
+
+THEME_FILE="$I3_DIR/themes/$1.config"
+CONFIG_DIR="$I3_DIR/configs"
+
+if [ -f $THEME_FILE ]; then
+  echo "Switching to $1 theme..."
+  cat $THEME_FILE $CONFIG_DIR/*.config > $I3_DIR/config
+  i3-msg restart &> /dev/null
+else
+  echo "File $THEME_FILE does not exist."
+fi
